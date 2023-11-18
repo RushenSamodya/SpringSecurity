04c06be7ff0a031c1a5b38c79bf4644293d5ea4d
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-11-18 07:25:49 +0530

    added the basic authentication techniques

--- SpringSecurity/src/main/java/com/rushen/SpringSecurity/Config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/rushen/SpringSecurity/Config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.rushen.SpringSecurity.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +36,22 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin,user);
 
     }
+
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/v1/auth/admin/login").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/api/v1/auth/user/login").hasAuthority("ROLE_USER")
+                        .anyRequest().authenticated()
+                )
+        .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
