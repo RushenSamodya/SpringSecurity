50dd5e4fe3614f50fe422f47e78fe2f583086afb
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-11-19 07:58:53 +0530

    Started with the vide and added a user and a db

---
+++ SpringSecurity/src/main/java/com/rushen/SpringSecurity/Entity/User.java
@@ -0,0 +1,24 @@
+package com.rushen.SpringSecurity.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String deliveryAddress;
+
+}
