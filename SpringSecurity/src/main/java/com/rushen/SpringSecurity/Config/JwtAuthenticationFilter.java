e31d813e204e1d1946655a582cd3e3991cd47fd5
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-11-20 07:02:19 +0530

    Daily commit 2
    completed the user entity,repo and the role and a bit of security config

---
+++ SpringSecurity/src/main/java/com/rushen/SpringSecurity/Config/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.rushen.SpringSecurity.Config;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @Nonnull HttpServletRequest request,
+            @Nonnull HttpServletResponse response,
+            @Nonnull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
+
+//41:43 Start from here next time
--- SpringSecurity/src/main/java/com/rushen/SpringSecurity/Entity/User.java
+++ SpringSecurity/src/main/java/com/rushen/SpringSecurity/Entity/User.java
@@ -3,6 +3,12 @@ package com.rushen.SpringSecurity.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -10,7 +16,7 @@ import lombok.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +26,42 @@ public class User {
     private String email;
     private String password;
     private String deliveryAddress;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+        //as we are getting the email as one of the login credentials.
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
