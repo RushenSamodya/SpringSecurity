298cfd226bb02cc3b4b07b3847ce93dbe806afe7
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-11-22 21:27:02 +0530

    did some really simple changes.

--- SpringSecurity/src/main/java/com/rushen/SpringSecurity/Config/JwtService.java
+++ SpringSecurity/src/main/java/com/rushen/SpringSecurity/Config/JwtService.java
@@ -1,19 +1,34 @@
 package com.rushen.SpringSecurity.Config;
 
-import com.auth0.jwt.interfaces.Claim;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "d2ffb8731436d2b925fcacb5cb110f2c56964f0b08afd016eaee5c09224f03d2";
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     private Key getSignInKey(){
@@ -21,15 +36,6 @@ public class JwtService {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    private Claim extractAllClaims(String token){
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    //Next Start 1:01:00
+    //Next Start 1:04:30
 
 }
